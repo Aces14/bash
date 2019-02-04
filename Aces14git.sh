f37d28fa871f83db1039c79b5c4b96996de6b86a
Aces14 <=>
2019-02-04 12:18:24 -0800

    AA

---
+++ Aces14git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  Aces14;
+git config --global user.name  Aces14;
+git config --global user.user Aces14;
+git config --global user.email javieracevedo460@yahoo.com;
+git config --list;
